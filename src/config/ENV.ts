39ed85fcf845dc7912ec6ab4d6367587dfdeae60
yusuken1121 <yusuken1121@gmail.com>
2025-02-16 22:38:39 -0800

    ⚡️ Toeic用の環境変数を追加

--- src/config/ENV.ts
+++ src/config/ENV.ts
@@ -1,3 +1,4 @@
 export const NOTION_API_KEY = process.env.NOTION_API_KEY;
 export const NOTION_DATABASE_ID = process.env.NOTION_DATABASE_ID;
 export const GEMINI_API_KEY = process.env.GEMINI_API_KEY;
+export const NOTION_DATABASE_TOEIC_ID = process.env.NOTION_DATABASE_TOEIC_ID;
