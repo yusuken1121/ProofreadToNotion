d94e4dd4346c2f348b7f21033a01ae9e446dc798
yusuken1121 <yusuken1121@gmail.com>
2025-03-03 23:06:11 -0800

    ⚡️ siteLinksを更新

--- src/config/siteLinks.ts
+++ src/config/siteLinks.ts
@@ -2,4 +2,5 @@ export const siteLinks = [
   { label: "ホーム", href: "/" },
   { label: "文章添削", href: "/proofread" },
   { label: "TOEIC解説", href: "/toeic" },
+  { label: "TOEIC読解", href: "/toeic/question-list" },
 ];
